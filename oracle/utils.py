90bb46aff14da7c4e6a1af0fb949d7ebf9657f89
Andy Casey <andycasey@gmail.com>
2014-08-26 16:58:42 +0100

    Don't need cachedproperties anymore

--- oracle/utils.py
+++ oracle/utils.py
@@ -10,31 +10,6 @@ import numpy as np
 from scipy.special import wofz
 
 
-class CachedProperty(object):    
-    """ Computes attribute value and caches it in the instance. """
-
-    def __init__(self, method, name=None):
-        self.method = method
-        self.name = name or method.__name__
-        self.__doc__ = method.__doc__
-    
-    def __get__(self, inst, cls): 
-        if inst is None:
-            return self
-        elif self.name in inst.__dict__:
-            return inst.__dict__[self.name]
-        else:
-            result = self.method(inst)
-            inst.__dict__[self.name]=result
-            return result    
-    
-    def __set__(self, inst, value):
-        raise AttributeError("This property is read-only")
-    
-    def __delete__(self,inst):
-        del inst.__dict__[self.name]
-
-
 def gaussian(mu, sigma, x):
     return np.exp(-(x - mu)**2 / (2*sigma**2))
 
